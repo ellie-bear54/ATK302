243abe04ef5340e1908a6e84845e497c62fb5b38
Lizzy <lizzy.fullness27@gmail.com>
2019-08-22 14:24:03 -0500

    meow

--- p5/circle/sketch.js
+++ p5/circle/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   // put setup code here
-  createCanvas(500, 500);
+  createCanvas(1000, 800);
 }
 
 function draw() {
   // put drawing code here
 
-  ellipse(mouseX, mouseY, 10, 10) ;
+  rect(mouseX, mouseY, 20, 30) ;
 }
