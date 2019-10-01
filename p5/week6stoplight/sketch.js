41ba0f73199c3a78012a1920b5335a765870d89c
Lizzy <lizzy.fullness27@gmail.com>
2019-10-01 17:24:12 -0500

    stoplite

--- p5/week6stoplight/sketch.js
+++ p5/week6stoplight/sketch.js
@@ -1,44 +1,64 @@
-//case 0 = green light on
-//case 1 = yellow light on
-//case 2= red light on
+var timer = 200;
 var turtle = 0;
 
 function setup() {
   createCanvas(1000, 1000);
-rectMode(CENTER);
-ellipseMode(CENTER);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
   // put setup code here
 }
 
 function draw() {
-  // put drawing code here
-//yellow rectangle
-//red ellipse
-//yellow ellipse
-//green ellipse
 
-fill("yellow") ;
-rect(width/2, height/2, 150, 500) ;
-
-fill("green") ;
-ellipse(width/2, height/2, 150, 150) ;
-
-fill("gray");
-ellipse(width/2, height/2 - 150, 150, 150) ;
-ellipse(width/2, height/2 + 150, 150, 150) ;
-switch (turtle) {
-
-case 0:
+  fill("yellow");
+  rect(width / 2, height / 2, 150, 500);
 
 
-break;
+  switch (turtle) {
+    case 0:
+      fill("grey");
+      ellipse(width / 2, height / 2 - 150, 150, 150);
+      ellipse(width / 2, height / 2, 150, 150);
 
-case 1:
-break;
+  
 
-case 2:
-break;
-}
+      fill("green");
+      ellipse(width / 2, height / 2 + 150, 150, 150);
+
+
+
+      break;
+
+    case 1:
+      fill("grey");
+      ellipse(width / 2, height / 2 + 150, 150, 150);
+      ellipse(width / 2, height / 2 - 150, 150, 150);
+
+      fill("yellow");
+
+      ellipse(width / 2, height / 2, 150, 150);
+
+      break;
+
+    case 2:
+      fill("red");
+      ellipse(width / 2, height / 2 - 150, 150, 150);
+
+
+
+      fill("grey");
+      ellipse(width / 2, height / 2 + 150, 150, 150);
+      ellipse(width / 2, height / 2, 150, 150);
+
+      break;
+  }
+
+  timer--;
+  if (timer < 0) {
+    timer = 200;
+    turtle = turtle + 1;
+  }
+
 
 
 }
